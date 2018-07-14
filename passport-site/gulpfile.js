967c92806631b64ea9063f2a90679989da1c8f05
Michael Young <michael.young@liferay.com>
2018-07-13 18:11:55 -0700

    create gulp build / move files to src dir

--- passport-site/gulpfile.js
+++ passport-site/gulpfile.js
@@ -1,9 +1,21 @@
 var gulp = require('gulp');
+var runSequence = require('run-sequence');
 var sass = require('gulp-sass');
 
+gulp.task('build', function(){
+	return gulp.src(['src/**/*', '!src/**/sass*/', '!src/**/sass*/**/*'])
+		.pipe(gulp.dest('dist/'));
+});
+
+gulp.task('dist', function (callback) {
+  runSequence(
+    'sass',
+    'build')
+});
+
 gulp.task('sass', function(){
-	return gulp.src('./sass/*.scss')
+	return gulp.src('src/sass/*.scss')
 		.pipe(sass())
-		.pipe(gulp.dest('./css'));
+		.pipe(gulp.dest('src/css'));
 });
 
