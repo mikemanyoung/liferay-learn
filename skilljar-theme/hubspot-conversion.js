3bbaca5f31af5fee5b7d8f72a097be284ef06291
Michael Young <michael.young@liferay.com>
2018-08-07 16:36:02 -0700

    externalize hubspot tracking and conversion code

---
+++ skilljar-theme/hubspot-conversion.js
@@ -0,0 +1,9 @@
+<!-- Hubspot Course Enrollment Tracking -->
+var _hsq = window._hsq = window._hsq || [];
+
+if (typeof(skilljarCourse) !== "undefined") {
+    _hsq.push(["trackEvent", {
+        id: "Skilljar Course Enrollment: "  + skilljarCourse.title,
+        value: null
+    }]);
+}
